c7382a92a12ae3561be9e5461ffc2086c489aec7
kushagra <kushagrajohra460@gmail.com>
2026-05-08 17:46:17 +0530

    add structure changes

--- index.js
+++ index.js
@@ -8,3 +8,10 @@
 // package.json me "devDependencies": { "nodemon": "^3.1.14" } add ho gaya h
 // ab hum package.json me "scripts": { "start": "node src/app.js", "dev": "nodemon src/app.js" } add karenge taki hum start command se server ko start kar sake aur dev command se server ko automatically restart kar sake jab bhi changes karenge
 //  "dev": "nodemon src/app.js" ab agar hum dev script run karenge toh server yeh src/app.js file ko watch karega aur jab bhi is file me changes honge toh server automatically restart ho jayega taki changes reflect ho sake
+
+// mkdir se hum folder bna skte h using terminal commands
+// eg source me flder bnao controllers ke naam se toh command hoga mkdir src/controllers
+// src me controllers folder bna diya h
+// mkdir utils mtlb ek utils folder bna diya h src ke andar
+// utils folder me hum helper functions rakh sakte h jo ki alag alag files me use honge
+// mtlb function ke jese joh baar baar use hote h unko hum utils folder me rakh sakte h taki code clean rahe aur reusability badh jaye
